3c8f9dc3b03102e150684d2a1cc4f1698dee68f2
Benedikt Schaible <benedikt.schaible@tuhh.de>
2021-06-15 17:00:03 +0200

    Add -B and -T flags which allow for relative Brightness and color Temperature changes as long as the light is already on

--- cmd/keylight/main.go
+++ cmd/keylight/main.go
@@ -16,11 +16,13 @@ func main() {
 	log.SetFlags(0)
 
 	var (
-		addr        = flag.String("a", "http://keylight:9123", "the address of an Elgato Key Light's HTTP API")
-		brightness  = flag.Int("b", 0, "set the brightness of a light to the specified percentage (valid: 3-100 %)")
-		display     = flag.String("d", "", "set the display name of an Elgato Key Light device")
-		info        = flag.Bool("i", false, "display the current status of an Elgato Key Light without changing its state")
-		temperature = flag.Int("t", 0, "set the color temperature of a light to the specified value (valid: 2900-7000 K)")
+		addr            = flag.String("a", "http://keylight:9123", "the address of an Elgato Key Light's HTTP API")
+		brightness      = flag.Int("b", 0, "set the brightness of a light to the specified percentage (valid: 3-100 %)")
+		brightnessincr  = flag.Int("B", 0, "change the brightness of a light by the specified percentage if light is on (valid: 3-100 %)")
+		display         = flag.String("d", "", "set the display name of an Elgato Key Light device")
+		info            = flag.Bool("i", false, "display the current status of an Elgato Key Light without changing its state")
+		temperature     = flag.Int("t", 0, "set the color temperature of a light to the specified value (valid: 2900-7000 K)")
+		temperatureincr = flag.Int("T", 0, "change the color temperature of a light by the specified value if light is on (valid: -4100-4100 K %)")
 	)
 	flag.Parse()
 
@@ -58,7 +60,8 @@ func main() {
 	}
 
 	// Only toggle the light if no modification flags are set.
-	toggle := *brightness == 0 && *temperature == 0
+	toggle := *brightness == 0 && *temperature == 0 && *brightnessincr == 0 && *temperatureincr == 0
+	toggleNonIncr := *brightness == 0 && *temperature == 0 && (*brightnessincr != 0 || *temperatureincr != 0)
 
 	for _, l := range lights {
 		if *brightness != 0 {
@@ -68,9 +71,34 @@ func main() {
 			l.Temperature = *temperature
 		}
 
+		// Only enable incremental brightness and color temperature adjustment when the light is on
+		if *brightnessincr != 0 {
+			switch {
+			case l.Brightness+*brightnessincr > 100:
+				l.Brightness = 100
+			case l.Brightness+*brightnessincr < 3:
+				l.Brightness = 3
+			default:
+				l.Brightness = l.Brightness + *brightnessincr
+			}
+		}
+		if *temperatureincr != 0 {
+			log.Printf("Received color temp Increment value %v, Current temp %v", *temperatureincr, l.Temperature)
+			switch {
+			case l.Temperature+*temperatureincr > 7000:
+				log.Println("Reached upper color temp bound")
+				l.Temperature = 7000
+			case l.Temperature+*temperatureincr < 2900:
+				log.Println("Reached lower color temp bound")
+				l.Temperature = 2900
+			default:
+				l.Temperature = l.Temperature + *temperatureincr
+			}
+		}
+
 		if toggle {
 			l.On = !l.On
-		} else {
+		} else if !toggleNonIncr {
 			// If the light is being modified, force it on.
 			l.On = true
 		}
